2517a6175fb164045c5e2a35e7736b92bf5ed4f5
Steven Hernandez <smhinfo@comcast.net>
2024-11-16 11:09:54 -0500

    created common and heateoas projects

---
+++ club-api-hateoas/src/main/java/com/smh/club/api/hateoas/ClubApiHateoasApplication.java
@@ -0,0 +1,13 @@
+package com.smh.club.api.hateoas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClubApiHateoasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClubApiHateoasApplication.class, args);
+	}
+
+}
---
+++ club-api-hateoas/src/test/java/com/smh/club/api/hateoas/ClubApiHateoasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.smh.club.api.hateoas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClubApiHateoasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
