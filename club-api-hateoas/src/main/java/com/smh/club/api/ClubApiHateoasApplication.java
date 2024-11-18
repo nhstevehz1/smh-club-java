656c4aa36bd9d582996563dff17fc546bfe93046
Steven Hernandez <smhinfo@comcast.net>
2024-11-18 10:15:26 -0500

    aligned with the new package scheme

--- club-api-hateoas/src/main/java/com/smh/club/api/ClubApiHateoasApplication.java
+++ club-api-hateoas/src/main/java/com/smh/club/api/ClubApiHateoasApplication.java
@@ -1,4 +1,4 @@
-package com.smh.club.api.hateoas;
+package com.smh.club.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
