ae7e81f31b42979a6163f88b70ecad701fe50d0f
Steven Hernandez <smhinfo@comcast.net>
2024-11-18 00:10:47 -0500

    finished member integration tests

--- club-data/src/test/java/com/smh/club/data/integration/persistence/MemberIntegrationTests.java
+++ club-data/src/test/java/com/smh/club/data/integration/persistence/MemberIntegrationTests.java
@@ -1,315 +1,278 @@
 package com.smh.club.data.integration.persistence;
 
+import com.smh.club.data.domain.entities.*;
+import com.smh.club.data.domain.repos.*;
 import io.zonky.test.db.AutoConfigureEmbeddedDatabase;
+import org.instancio.Instancio;
+import org.instancio.junit.InstancioExtension;
+import org.instancio.junit.WithSettings;
+import org.instancio.settings.Keys;
+import org.instancio.settings.Settings;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.util.List;
+
 import static io.zonky.test.db.AutoConfigureEmbeddedDatabase.DatabaseProvider.ZONKY;
+import static org.instancio.Select.all;
+import static org.instancio.Select.field;
+import static org.junit.jupiter.api.Assertions.*;
 
 
 @ActiveProfiles("tests")
 @RunWith(SpringRunner.class)
+@ExtendWith(InstancioExtension.class)
 @DataJpaTest
 @AutoConfigureEmbeddedDatabase (
         provider = ZONKY,
         type = AutoConfigureEmbeddedDatabase.DatabaseType.POSTGRES,
         refresh = AutoConfigureEmbeddedDatabase.RefreshMode.AFTER_EACH_TEST_METHOD)
-public class MemberIntegrationTests extends PersistenceTestsBase {
-/*
-    @SuppressWarnings("unused")
-    @Autowired
-    private MembersRepo membersRepo;
+public class MemberIntegrationTests {
 
-    @SuppressWarnings("unused")
+    @Autowired
+    private MembersRepo memberRepo;
+
     @Autowired
     private AddressRepo addressRepo;
 
-    @SuppressWarnings("unused")
     @Autowired
     private EmailRepo emailRepo;
 
-    @SuppressWarnings("unused")
     @Autowired
     private PhoneRepo phoneRepo;
 
-    @SuppressWarnings("unused")
     @Autowired
     private RenewalsRepo renewalsRepo;
 
-    @Test
-    public void saveMember_Success() {
-        // setup
-        var joinedDate = LocalDate.now();
-        var birthDate = joinedDate.minusYears(30L);
-        var m1 = createMember(0, birthDate, joinedDate);
-
-        // execute
-        membersRepo.save(m1);
-        var id = m1.getId();
-        var member = membersRepo.findById(id).orElseThrow();
-
-        // verify
-        assertEquals(id, member.getId(), "Id's don't match");
-        verifyMember(0, member, birthDate, joinedDate);
-    }
+    @WithSettings
+    private final Settings settings = Settings.create()
+            .set(Keys.SET_BACK_REFERENCES, true)
+            .set(Keys.JPA_ENABLED, true)
+            .set(Keys.COLLECTION_MAX_SIZE, 0);
 
     @ParameterizedTest
-    @ValueSource(ints = {5, 10, 15, 20})
-    public void saveMembers_Success(int size) {
+    @ValueSource(ints = {5, 10, 20})
+    public void save_memberNumber_not_unique_throws(int size) {
         // setup
-        var joinedDateBase = LocalDate.now();
-        var birthDateBase = joinedDateBase.minusYears(30L);
-        var members = createMembers(size, birthDateBase, joinedDateBase);
+        var members = createList(size);
+        var number = memberRepo.saveAll(members).get((int)Math.ceil(size / 2d)).getMemberNumber();
+        var member = createEntity();
+        member.setMemberNumber(number);
 
-        // execute
-        membersRepo.saveAll(members);
-        var sortedMembers = membersRepo.findAll(Sort.by("id").ascending());
-
-        // verify
-        assertEquals(size, sortedMembers.size(), "Member list size does not match");
-        for (int ii = 0; ii < size; ii++) {
-            verifyMember(ii, sortedMembers.get(ii), birthDateBase, joinedDateBase);
-        }
+        // execute and verify
+        assertThrows(Exception.class, () -> memberRepo.save(member));
     }
 
     @Test
-    public void saveMember_MemberNumberNotUniqueThrowsException() {
+    public void save_firstName_is_null_throws() {
         // setup
-        var m1 = createMember(0, LocalDate.now(), LocalDate.now());
-        var m2 = createMember(0, LocalDate.now(), LocalDate.now());
-        m2.setMemberNumber(m1.getMemberNumber());
-
-        // Execute and verify
-        membersRepo.save(m1);
-        assertThrows(Exception.class, () -> membersRepo.save(m2));
-    }
-
-    @Test
-    public void saveMember_FirstNameIsNullThrowsException() {
-        // setup
-        var member = createMember(0, LocalDate.now(), LocalDate.now());
+        var member = createEntity();
         member.setFirstName(null);
 
         // execute and verify
-        assertThrows(Exception.class, () -> membersRepo.save(member));
+        assertThrows(Exception.class, () -> memberRepo.save(member));
     }
 
     @Test
-    public void saveMember_LastNameIsNullThrowsException() {
+    public void save_middleName_is_null_does_not_throws() {
         // setup
-        var member = createMember(0, LocalDate.now(), LocalDate.now());
+        var member = createEntity();
+        member.setMiddleName(null);
+
+        // execute and verify
+        memberRepo.save(member);
+    }
+
+    @Test
+    public void save_lastName_is_null_throws() {
+        // setup
+        var member = createEntity();
         member.setLastName(null);
 
         // execute and verify
-        assertThrows(Exception.class, () -> membersRepo.save(member));
+        assertThrows(Exception.class, () -> memberRepo.save(member));
     }
 
     @Test
-    public void saveMember_MiddleNameIsNullDoesNotThrowsException() {
+    public void save_suffix_is_null_does_not_throws() {
         // setup
-        var m1 = createMember(0, LocalDate.now(), LocalDate.now());
-        m1.setMiddleName(null);
+        var member = createEntity();
+        member.setSuffix(null);
 
-        // execute
-        var member = membersRepo.save(m1);
-
-        // verify
-        assertNull(member.getMiddleName(), "MiddleName is not null");
+        // execute and verify
+        memberRepo.save(member);
     }
 
     @Test
-    public void saveMember_SuffixIsNullDoesNotThrowsException() {
+    public void save_birthDate_is_null_throws() {
         // setup
-        var m1 = createMember(0, LocalDate.now(), LocalDate.now());
-        m1.setSuffix(null);
-
-        // execute
-        var member = membersRepo.save(m1);
-
-        // verify
-        assertNull(member.getSuffix(), "Suffix is not null");
-    }
-
-    @Test
-    public void saveMember_BirthDateIsNullThrowsException() {
-        // setup
-        var member = createMember(0, LocalDate.now(), LocalDate.now());
+        var member = createEntity();
         member.setBirthDate(null);
 
         // execute and verify
-        assertThrows(Exception.class, () -> membersRepo.save(member));
+        assertThrows(Exception.class, () -> memberRepo.save(member));
     }
 
     @Test
-    public void saveMember_JoinedDateIsNullThrowsException() {
+    public void save_joinedDate_is_null_throws() {
         // setup
-        var member = createMember(0, LocalDate.now(), LocalDate.now());
+        var member = createEntity();
         member.setJoinedDate(null);
 
         // execute and verify
-        assertThrows(Exception.class, () -> membersRepo.save(member));
+        assertThrows(Exception.class, () -> memberRepo.save(member));
     }
 
-    @Test
-    public void memberNumberExists_ReturnsTrue() {
+    @ParameterizedTest
+    @ValueSource(ints = {5,20,50})
+    public void get_memberNumbers_matches(int size) {
         // setup
-        var localDate = LocalDate.now();
-        var members = List.of(
-          createMember(1, localDate, localDate),
-          createMember(5 ,localDate, localDate),
-          createMember(10, localDate, localDate)
-        );
+        var members = memberRepo.saveAll(createList(size));
+        var numbers = members.stream().map(MemberEntity::getMemberNumber).toList();
+        var highest = members.stream().map(MemberEntity::getMemberNumber).max(Integer::compareTo).orElseThrow();
 
         // execute
-        var memberList = membersRepo.saveAll(members);
+        var ret = memberRepo.findByMemberNumberGreaterThanEqualAndMemberNumberLessThanEqual(0, highest);
+        var vals = ret.stream().map(MemberNumberOnly::getMemberNumber).toList();
 
-        // verify
-        assertEquals(3, memberList.size(), "Member list size doesn't match");
-        assertTrue(membersRepo.existsByMemberNumber(1));
-        assertTrue(membersRepo.existsByMemberNumber(5));
-        assertTrue(membersRepo.existsByMemberNumber(10));
+        // assert
+        vals.forEach(v -> assertTrue(numbers.contains(v)));
     }
 
-    @Test
-    public void memberNumberExists_ReturnsFalse() {
+    @ParameterizedTest
+    @ValueSource(ints = {5, 10, 20})
+    public void save_with_children_success(int size) {
         // setup
-        var localDate = LocalDate.now();
-        var members = List.of(
-                createMember(1, localDate, localDate),
-                createMember(5 ,localDate, localDate),
-                createMember(10, localDate, localDate)
-        );
+        var members = memberRepo.saveAll(createListWithChildren(size));
 
         // execute
-        var memberList = membersRepo.saveAll(members);
+        var ret = memberRepo.findAll();
 
-        // verify
-        assertEquals(3, memberList.size(), "Member list size doesn't match");
-        assertFalse(membersRepo.existsByMemberNumber(2));
-        assertFalse(membersRepo.existsByMemberNumber(6));
-        assertFalse(membersRepo.existsByMemberNumber(11));
+        //verify
+        assertEquals(members, ret);
     }
 
-
-    @Test
-    public void saveMemberWithChildren() {
+    @ParameterizedTest
+    @ValueSource(ints = {5, 10, 20})
+    public void delete_member_deletes_children(int size) {
         // setup
-        var m1 = createMember(0, LocalDate.now(), LocalDate.now());
-        m1.addAddress(createAddress(0, AddressType.Work));
-        m1.addPhone(createPhone(0, PhoneType.Work));
-        m1.addEmail(createEmail(0, EmailType.Work));
-        m1.addRenewal(createRenewal(0, LocalDate.now()));
-
-        var id = membersRepo.save(m1).getId();
+        var member = memberRepo.saveAllAndFlush(createListWithChildren(size)).get((int)Math.ceil(size / 2d));
 
         // execute
-        var member = membersRepo.findById(id).orElseThrow();
-
+        memberRepo.deleteById(member.getId());
 
         // verify
-        assertEquals(1, addressRepo.count());
-        assertEquals(1, emailRepo.count());
-        assertEquals(1, phoneRepo.count());
-        assertEquals(1, renewalsRepo.count());
-        assertEquals(1, member.getAddresses().size());
-        assertEquals(1, member.getEmails().size());
-        assertEquals(1, member.getPhones().size());
-        assertEquals(1, member.getRenewals().size());
+        assertFalse(memberRepo.existsById(member.getId()));
+        assertFalse(addressRepo.existsById(member.getAddresses().get(0).getId()));
+        assertFalse(emailRepo.existsById(member.getEmails().get(0).getId()));
+        assertFalse(phoneRepo.existsById(member.getPhones().get(0).getId()));
+        assertFalse(renewalsRepo.existsById(member.getRenewals().get(0).getId()));
     }
 
-    @Test
-    public void deleteMemberDeletesChildren() {
+    @ParameterizedTest
+    @ValueSource(ints = {5, 10, 20})
+    public void delete_children_save_member(int size) {
         // setup
-        var m1 = createMember(0, LocalDate.now(), LocalDate.now());
-        m1.addAddress(createAddress(0, AddressType.Work));
-        m1.addPhone(createPhone(0, PhoneType.Work));
-        m1.addEmail(createEmail(0, EmailType.Work));
-        m1.addRenewal(createRenewal(0, LocalDate.now()));
+        var member = memberRepo.saveAllAndFlush(createListWithChildren(size)).get((int)Math.ceil(size / 2d));
+        var address = member.getAddresses().get(0);
+        var email = member.getEmails().get(0);
+        var phone = member.getPhones().get(0);
+        var renewal = member.getRenewals().get(0);
 
-        var id = membersRepo.save(m1).getId();
+        member.removeAddress(address);
+        member.removeEmail(email);
+        member.removePhone(phone);
+        member.removeRenewal(renewal);
 
         // execute
-        membersRepo.deleteById(id);
+        var ret = memberRepo.save(member);
 
         // verify
-        assertEquals(0, membersRepo.count());
-        assertEquals(0, addressRepo.count());
-        assertEquals(0, emailRepo.count());
-        assertEquals(0, phoneRepo.count());
-        assertEquals(0, renewalsRepo.count());
+        assertTrue(ret.getAddresses().isEmpty());
+        assertTrue(ret.getEmails().isEmpty());
+        assertTrue(ret.getPhones().isEmpty());
+        assertTrue(ret.getRenewals().isEmpty());
+        assertFalse(addressRepo.existsById(address.getId()));
+        assertFalse(emailRepo.existsById(email.getId()));
+        assertFalse(phoneRepo.existsById(phone.getId()));
+        assertFalse(renewalsRepo.existsById(renewal.getId()));
     }
 
-    @Test
-    public void saveMemberDeleteChildren() {
+    @ParameterizedTest
+    @ValueSource(ints = {5, 10, 20})
+    public void findMemberByMemberNumber(int size) {
         // setup
-        var m1 = createMember(0, LocalDate.now(), LocalDate.now());
-        m1.addAddress(createAddress(0, AddressType.Work));
-        m1.addEmail(createEmail(0, EmailType.Other));
-        m1.addPhone(createPhone(0, PhoneType.Mobile));
-        m1.addRenewal(createRenewal(0, LocalDate.now()));
-        var id = membersRepo.save(m1).getId();
+        var member = memberRepo.saveAllAndFlush(createListWithChildren(size)).get((int)Math.ceil(size / 2d));
 
         // execute
-        var member = membersRepo.getReferenceById(id);
-        member.removeAddress(member.getAddresses().get(0));
-        member.removeEmail(member.getEmails().get(0));
-        member.removePhone(member.getPhones().get(0));
-        member.removeRenewal(member.getRenewals().get(0));
-        membersRepo.save(member);
-        var updated = membersRepo.findAll().get(0);
+        var ret = memberRepo.findByMemberNumber(member.getMemberNumber());
 
         // verify
-        assertEquals(0, updated.getAddresses().size());
-        assertEquals(0, updated.getEmails().size());
-        assertEquals(0, updated.getPhones().size());
-        assertEquals(0, member.getRenewals().size());
-        assertEquals(0, addressRepo.count());
-        assertEquals(0, emailRepo.count());
-        assertEquals(0, phoneRepo.count());
-        assertEquals(0, renewalsRepo.count());
+        assertTrue(ret.isPresent());
+        assertEquals(member, ret.get());
     }
 
-    @Test
-    public void findMemberByMemberNumber() {
-        // setup
-        var members = List.of(
-                createMember(1, LocalDate.now(), LocalDate.now()),
-                createMember(5, LocalDate.now(), LocalDate.now()),
-                createMember(10, LocalDate.now(), LocalDate.now())
-        );
-        membersRepo.saveAll(members);
-
-        // execute
-        var member = membersRepo.findByMemberNumber(5);
-
-        // verify
-        assertTrue(member.isPresent());
-        assertEquals(5, member.get().getMemberNumber());
+    private MemberEntity createEntity() {
+        return Instancio.of(MemberEntity.class)
+                .ignore(field(MemberEntity::getId))
+                .create();
     }
 
-    @Test
-    public void findMemberNumberRange() {
-        // setup
-        var members = new ArrayList<MemberEntity>();
-        for (int ii = 0; ii < 15; ii++ ) {
-            if(ii % 3 == 0) {
-                members.add(createMember(ii, LocalDate.now(), LocalDate.now()));
-            }
-        }
-        membersRepo.saveAll(members);
+    private List<MemberEntity> createList(int size) {
+        return Instancio.ofList(MemberEntity.class)
+                .size(size)
+                .ignore(field(MemberEntity::getId))
+                .withUnique(field(MemberEntity::getMemberNumber))
+                .create();
+    }
 
-        // execute
-        var memberNumbers =
-                membersRepo.findByMemberNumberGreaterThanEqualAndMemberNumberLessThanEqual(6, 12);
+    private List<MemberEntity> createListWithChildren(int size) {
+        return Instancio.ofList(MemberEntity.class)
+                .size(size)
+                .ignore(field(MemberEntity::getId))
+                .withUnique(field(MemberEntity::getMemberNumber))
+                .onComplete(all(MemberEntity.class), (MemberEntity member) -> {
+                    member.addAddress(createAddress());
+                    member.addEmail(createEmail());
+                    member.addPhone(createPhone());
+                    member.addRenewal(createRenewal());
+                })
+                .create();
+    }
 
-        // verify
-        assertEquals(3, memberNumbers.size());
-        assertFalse(memberNumbers.stream().anyMatch(mn -> mn.getMemberNumber() == 3));
-        assertTrue(memberNumbers.stream().anyMatch(mn -> mn.getMemberNumber() == 6));
-        assertTrue(memberNumbers.stream().anyMatch(mn -> mn.getMemberNumber() == 9));
-        assertTrue(memberNumbers.stream().anyMatch(mn -> mn.getMemberNumber() == 12));
-        assertFalse(memberNumbers.stream().anyMatch(mn -> mn.getMemberNumber() == 15));
-    }*/
-}
+    private AddressEntity createAddress() {
+        return Instancio.of(AddressEntity.class)
+                .ignore(field(AddressEntity::getId))
+                .ignore(field(AddressEntity::getMember))
+                .create();
+    }
+
+    private EmailEntity createEmail() {
+        return Instancio.of(EmailEntity.class)
+                .ignore(field(EmailEntity::getId))
+                .ignore(field(EmailEntity::getMember))
+                .create();
+    }
+
+    private PhoneEntity createPhone() {
+        return Instancio.of(PhoneEntity.class)
+                .ignore(field(PhoneEntity::getId))
+                .ignore(field(PhoneEntity::getMember))
+                .create();
+    }
+
+    private RenewalEntity createRenewal() {
+        return Instancio.of(RenewalEntity.class)
+                .ignore(field(RenewalEntity::getId))
+                .ignore(field(RenewalEntity::getMember))
+                .create();
+    }
+
+ }
