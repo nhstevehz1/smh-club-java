9f1f3d5442b204f15d939df92866796d35a15b03
Steven Hernandez <smhinfo@comcast.net>
2024-11-24 09:35:53 -0500

    fixed a build error

--- club-api-hateoas/src/main/java/com/smh/club/api/hateoas/config/MapperConfig.java
+++ club-api-hateoas/src/main/java/com/smh/club/api/hateoas/config/MapperConfig.java
@@ -9,7 +9,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 /**
- * Configuration for {@link ModelMapper.}
+ * Configuration for {@link ModelMapper}
  */
 @Configuration
 public class MapperConfig {
