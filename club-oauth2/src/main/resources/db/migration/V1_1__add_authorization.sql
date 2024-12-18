cc96382706c78f1fc857a29d25183aaee8f56422
Steven Hernandez <smhinfo@comcast.net>
2024-12-18 13:08:12 -0500

    Refactored schemas Added Auth consent tests

--- club-oauth2/src/main/resources/db/migration/V1_1__add_authorization.sql
+++ club-oauth2/src/main/resources/db/migration/V1_1__add_authorization.sql
@@ -45,21 +45,3 @@ CREATE TABLE auth.token_scopes_set (
         FOREIGN KEY (auth_id, token_type)
             REFERENCES auth.authorization_token (auth_id, token_type)
 );
-
-CREATE TABLE auth.authorization_consent (
-    registered_client_id varchar(50)  NOT NULL,
-    principal_name       varchar(50)  NOT NULL,
-
-    CONSTRAINT pk_auth_consent
-        PRIMARY KEY (registered_client_id, principal_name)
-);
-
-CREATE TABLE auth.authorization_consent_authorities_set (
-    registered_client_id varchar(50) NOT NULL,
-    principal_name varchar(50) NOT NULL,
-    authority varchar(100) NOT NULL,
-
-    CONSTRAINT fk_reg_client_id__principal_name___authorization_consent
-        FOREIGN KEY (registered_client_id, principal_name)
-            REFERENCES auth.authorization_consent (registered_client_id, principal_name)
-)
--- club-oauth2/src/test/java/smh/club/oauth2/services/ConsentServiceIntegrationTests.java
+++ club-oauth2/src/test/java/smh/club/oauth2/services/ConsentServiceIntegrationTests.java
@@ -1,4 +1,155 @@
 package smh.club.oauth2.services;
 
+import io.zonky.test.db.AutoConfigureEmbeddedDatabase;
+import jakarta.transaction.Transactional;
+import java.util.UUID;
+import java.util.stream.Collectors;
+import org.instancio.Instancio;
+import org.instancio.junit.InstancioExtension;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.dao.DataRetrievalFailureException;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.oauth2.server.authorization.OAuth2AuthorizationConsent;
+import org.springframework.security.oauth2.server.authorization.OAuth2AuthorizationConsentService;
+import org.springframework.test.context.ActiveProfiles;
+import smh.club.oauth2.contracts.mappers.AuthorizationConsentMapper;
+import smh.club.oauth2.domain.entities.AuthorizationConsentEntity;
+import smh.club.oauth2.domain.entities.ClientEntity;
+import smh.club.oauth2.domain.repos.AuthorizationConsentRepository;
+import smh.club.oauth2.domain.repos.ClientRepository;
+
+import static io.zonky.test.db.AutoConfigureEmbeddedDatabase.DatabaseProvider.ZONKY;
+import static org.instancio.Select.field;
+import static org.junit.jupiter.api.Assertions.*;
+
+@ActiveProfiles({"tests", "prod"})
+@Transactional
+@ExtendWith(InstancioExtension.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.NONE)
+@AutoConfigureEmbeddedDatabase(
+    provider = ZONKY,
+    type = AutoConfigureEmbeddedDatabase.DatabaseType.POSTGRES,
+    refresh = AutoConfigureEmbeddedDatabase.RefreshMode.AFTER_EACH_TEST_METHOD)
 public class ConsentServiceIntegrationTests {
+
+  @Autowired
+  private AuthorizationConsentRepository consentRepo;
+
+  @Autowired
+  private ClientRepository clientRepo;
+
+  @Autowired
+  private AuthorizationConsentMapper mapperMock;
+
+  @Autowired
+  private OAuth2AuthorizationConsentService service;
+
+  @Test
+  public void save_success() {
+    // setup
+    var consent = createAuthConsent();
+
+    // execute
+    service.save(consent);
+
+    // verify
+    var ret = consentRepo
+        .findByRegisteredClientIdAndPrincipalName(consent.getRegisteredClientId(),
+            consent.getPrincipalName());
+
+    assertTrue(ret.isPresent());
+  }
+
+  @Test
+  public void remove_success() {
+    // setup
+    var consent = createAuthConsent();
+    var entity = createAuthConsentEntity(consent);
+
+    consentRepo.save(entity);
+
+    // execute
+    service.remove(consent);
+
+    // verify
+    var ret = consentRepo
+        .findByRegisteredClientIdAndPrincipalName(consent.getRegisteredClientId(),
+            consent.getPrincipalName());
+
+    assertTrue(ret.isEmpty());
+  }
+
+  @Test
+  public void findById_success_returns_authConsent() {
+    // setup
+    var consent = createAuthConsent();
+    var entity = createAuthConsentEntity(consent);
+    var client = createClientEntity(consent);
+
+    consentRepo.save(entity);
+    clientRepo.save(client);
+
+    // execute
+    var ret = service.findById(consent.getRegisteredClientId(), consent.getPrincipalName());
+
+    // verify
+    assertNotNull(ret);
+    assertEquals(consent, ret);
+  }
+
+  @Test
+  public void findById_returns_null_when_consent_does_not_exist() {
+    // setup
+    var consent = createAuthConsent();
+
+    // execute
+    var ret = service.findById(consent.getRegisteredClientId(), consent.getPrincipalName());
+
+    // verify
+    assertNull(ret);
+  }
+
+  @Test
+  public void findById_throws_when_registeredClient_does_not_exist() {
+    // setup
+    var consent = createAuthConsent();
+    var entity = createAuthConsentEntity(consent);
+
+    consentRepo.save(entity);
+
+    // execute and verify
+    assertThrows(DataRetrievalFailureException.class,
+        () -> service.findById(consent.getRegisteredClientId(), consent.getPrincipalName()));
+  }
+
+  private OAuth2AuthorizationConsent createAuthConsent() {
+    var clientId = UUID.randomUUID().toString();
+    var principalName = Instancio.create(String.class);
+
+    return OAuth2AuthorizationConsent.withId(clientId, principalName)
+        .authority(new SimpleGrantedAuthority("USER"))
+        .build();
+  }
+
+  private AuthorizationConsentEntity createAuthConsentEntity(OAuth2AuthorizationConsent consent) {
+    var authorities = consent.getAuthorities()
+        .stream().map(GrantedAuthority::getAuthority).collect(Collectors.toSet());
+
+    return
+        AuthorizationConsentEntity.builder()
+            .registeredClientId(consent.getRegisteredClientId())
+            .principalName(consent.getPrincipalName())
+            .authorities(authorities)
+            .build();
+  }
+
+  private ClientEntity createClientEntity(OAuth2AuthorizationConsent consent) {
+    return Instancio.of(ClientEntity.class)
+        .set(field(ClientEntity::getId), consent.getRegisteredClientId())
+        .create();
+  }
 }
