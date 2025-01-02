c516e3ba20020de0ba39801026d145bd46916c6c
Steven Hernandez <smhinfo@comcast.net>
2025-01-02 12:08:27 -0500

    added shells for components

---
+++ club-frontend-angular/src/app/shared/components/spinner-dialog/spinner-dialog.component.ts
@@ -0,0 +1,11 @@
+import {Component} from '@angular/core';
+
+@Component({
+  selector: 'app-spinner-dialog',
+  imports: [],
+  templateUrl: './spinner-dialog.component.html',
+  styleUrl: './spinner-dialog.component.css'
+})
+export class SpinnerDialogComponent {
+
+}
