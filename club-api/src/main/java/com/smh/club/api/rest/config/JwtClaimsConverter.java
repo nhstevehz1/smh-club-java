6316ddf60e5ff6e800748e7e3823da259d1aa2f5
Steven Hernandez <smhinfo@comcast.net>
2025-01-26 12:13:52 -0500

    cleaned up code issues

--- club-api/src/main/java/com/smh/club/api/rest/config/JwtClaimsConverter.java
+++ club-api/src/main/java/com/smh/club/api/rest/config/JwtClaimsConverter.java
@@ -5,6 +5,7 @@ import java.util.Map;
 import java.util.Set;
 import java.util.stream.Collectors;
 import org.springframework.core.convert.converter.Converter;
+import org.springframework.lang.NonNull;
 import org.springframework.security.authentication.AbstractAuthenticationToken;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
@@ -16,7 +17,7 @@ import org.springframework.stereotype.Component;
 public class JwtClaimsConverter implements Converter<Jwt, AbstractAuthenticationToken> {
 
   @Override
-  public AbstractAuthenticationToken convert( Jwt jwt) {
+  public AbstractAuthenticationToken convert(@NonNull Jwt jwt) {
     var authorities = extractRealmRoles(jwt);
     return new JwtAuthenticationToken(jwt, authorities, getPrincipalFromClaim(jwt));
   }
@@ -26,9 +27,11 @@ public class JwtClaimsConverter implements Converter<Jwt, AbstractAuthentication
     return jwt.getClaim(claimName);
   }
 
+  @SuppressWarnings("unchecked")
   private Collection<GrantedAuthority> extractRealmRoles(Jwt jwt) {
     Map<String, Object> resource = jwt.getClaim("realm_access");
     Collection<String> roles;
+
     if (resource == null
         || (roles = (Collection<String>) resource.get("roles")) == null) {
       return Set.of();
