e288a519906367a270dc555555d5095cdf5e0c63
Steven Hernandez <smhinfo@comcast.net>
2025-01-28 09:51:04 -0500

    removed unused imports

--- club-api/src/main/java/com/smh/club/api/rest/config/MethodSecurityConfig.java
+++ club-api/src/main/java/com/smh/club/api/rest/config/MethodSecurityConfig.java
@@ -2,7 +2,6 @@ package com.smh.club.api.rest.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.env.Environment;
 import org.springframework.security.access.expression.method.DefaultMethodSecurityExpressionHandler;
 import org.springframework.security.access.expression.method.MethodSecurityExpressionHandler;
 import org.springframework.security.access.hierarchicalroles.RoleHierarchy;
