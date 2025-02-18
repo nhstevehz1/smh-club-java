c03f5f4a247a200da81cb127203fb7433d52b41e
Steven Hernandez <smhinfo@comcast.net>
2025-02-18 09:44:23 -0500

    Added success component

---
+++ club-frontend-angular/src/app/shared/components/success/success.component.ts
@@ -0,0 +1,35 @@
+import {Component, EventEmitter, Input, Output} from '@angular/core';
+import {MatCardTitle} from "@angular/material/card";
+import {MatFabButton} from "@angular/material/button";
+import {MatTooltip} from "@angular/material/tooltip";
+import {MatIcon} from "@angular/material/icon";
+
+@Component({
+  selector: 'app-success',
+  imports: [
+    MatCardTitle,
+    MatIcon,
+    MatTooltip,
+    MatFabButton
+  ],
+  templateUrl: './success.component.html',
+  styleUrl: './success.component.scss'
+})
+export class SuccessComponent {
+
+  @Input()
+  public successMessage = 'Success';
+
+  @Input()
+  public iconName = 'checked';
+
+  @Input()
+  public buttonToolTip = 'Ok';
+
+  @Output()
+  public buttonClick: EventEmitter<any> = new EventEmitter();
+
+  onButtonClicked(): void {
+    this.buttonClick.next(null);
+  }
+}
