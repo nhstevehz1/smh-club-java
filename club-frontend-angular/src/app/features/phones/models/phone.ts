eff5aa412ad3c4c246b28ee23b69c14feb716fff
Steven Hernandez <smhinfo@comcast.net>
2025-02-23 13:28:49 -0500

    fixed unit tests

--- club-frontend-angular/src/app/features/phones/models/phone.ts
+++ club-frontend-angular/src/app/features/phones/models/phone.ts
@@ -1,7 +1,9 @@
+import { PhoneType } from "./phone-type";
+
 export interface Phone {
     id: number;
     member_id: number;
     country_code: string;
     phone_number: string;
-    phone_type: string;
+    phone_type: PhoneType | string;
 }
