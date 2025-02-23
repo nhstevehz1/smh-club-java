97568492823dd556230230ca11e237f4b7c3df17
Steven Hernandez <smhinfo@comcast.net>
2025-02-23 13:53:32 -0500

    removed test code

--- club-frontend-angular/src/app/core/auth/factories/auth-app-init-factory.ts
+++ club-frontend-angular/src/app/core/auth/factories/auth-app-init-factory.ts
@@ -4,13 +4,3 @@ export function authAppInitFactory(authService: AuthService): () => Promise<void
     console.log('running auth init factor');
     return () => authService.startupLoginSequence().then(() => console.log('startup then'));
 }
-
-export function appInitTest(): () => Promise<void> {
-    myTest();
-    return () => Promise.resolve();
-
-}
-
-export function myTest(): void {
-    console.log('Im in myTest');
-}
