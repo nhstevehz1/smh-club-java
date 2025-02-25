3b3412c143d64dc3dcd6dc185b2cf4149d79c384
Steven Hernandez <smhinfo@comcast.net>
2025-02-25 16:15:20 -0500

    changed logging statement

--- club-frontend-angular/src/app/core/auth/factories/auth-app-init-factory.ts
+++ club-frontend-angular/src/app/core/auth/factories/auth-app-init-factory.ts
@@ -1,6 +1,6 @@
 import {AuthService} from "../services/auth.service";
 
 export function authAppInitFactory(authService: AuthService): () => Promise<void> {
-    console.log('running auth init factor');
-    return () => authService.startupLoginSequence().then(() => console.log('startup then'));
+    console.debug('running auth init factory');
+    return () => authService.startupLoginSequence().then(() => console.debug('startup then'));
 }
