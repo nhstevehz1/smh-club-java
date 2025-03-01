8b6a5e7c737bf38b46979fcf704f933cddabb328
Steven Hernandez <smhinfo@comcast.net>
2025-03-01 07:47:21 -0500

    i18n home

--- club-frontend-angular/src/app/features/home/home.component.ts
+++ club-frontend-angular/src/app/features/home/home.component.ts
@@ -1,12 +1,14 @@
 import {Component} from '@angular/core';
 import {MatCardModule} from "@angular/material/card";
 import {MatIconModule} from "@angular/material/icon";
+import {TranslatePipe} from "@ngx-translate/core";
 
 @Component({
   selector: 'app-home',
     imports: [
         MatCardModule,
-        MatIconModule
+        MatIconModule,
+        TranslatePipe
     ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
