d41ea6fee8894d4b0bb5bc63d789ba08d3edf95d
Steven Hernandez <smhinfo@comcast.net>
2025-03-04 11:13:44 -0500

    removed unused directive

--- club-frontend-angular/src/app/shared/directives/conditional.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import {ConditionalDirective} from './conditional.directive';
-
-describe('ConditionalDirective', () => {
-  it('should create an instance', () => {
-    const directive = new ConditionalDirective();
-    expect(directive).toBeTruthy();
-  });
-});
