f07664a7c0ab6a090866d34760f32fa7c25ae167
Steven Hernandez <smhinfo@comcast.net>
2025-03-12 21:52:04 -0400

    fixed unit test bugs

--- club-frontend-angular/src/app/core/layout/header/header.component.spec.ts
+++ club-frontend-angular/src/app/core/layout/header/header.component.spec.ts
@@ -171,7 +171,7 @@ describe('HeaderComponent', () => {
       const menuHarness = await loader.getHarness(MatMenuHarness);
       await menuHarness.open();
 
-      const profileHarness = await menuHarness.getItems({text: 'personProfile'});
+      const profileHarness = await menuHarness.getItems({text: 'personlayout.header.profile'});
 
       expect(profileHarness.length).toEqual(1);
     });
@@ -180,7 +180,7 @@ describe('HeaderComponent', () => {
       const menuHarness = await loader.getHarness(MatMenuHarness);
       await menuHarness.open();
 
-      const logoutHarness = await menuHarness.getItems({text: 'exit_to_appLogout'});
+      const logoutHarness = await menuHarness.getItems({text: 'exit_to_applayout.header.logout'});
 
       expect(logoutHarness.length).toEqual(1);
     });
@@ -190,7 +190,7 @@ describe('HeaderComponent', () => {
       const menuHarness = await loader.getHarness(MatMenuHarness);
       await menuHarness.open();
 
-      await menuHarness.clickItem({text: 'personProfile'});
+      await menuHarness.clickItem({text: 'personlayout.header.profile'});
 
       expect(spy).toHaveBeenCalled();
     });
@@ -200,7 +200,7 @@ describe('HeaderComponent', () => {
       const menuHarness = await loader.getHarness(MatMenuHarness);
       await menuHarness.open();
 
-      await menuHarness.clickItem({text: 'exit_to_appLogout'});
+      await menuHarness.clickItem({text: 'exit_to_applayout.header.logout'});
 
       expect(spy).toHaveBeenCalled();
     });
