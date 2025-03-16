59209039b5e88676e1c7c14f3e3c354371ba69bc
Steven Hernandez <smhinfo@comcast.net>
2025-03-16 12:52:35 -0400

    created common title page component

---
+++ club-frontend-angular/src/app/shared/components/title-page/title-page.component.spec.ts
@@ -0,0 +1,88 @@
+import {ComponentFixture, TestBed} from '@angular/core/testing';
+
+import {TitlePageComponent} from './title-page.component';
+import {TranslateModule} from "@ngx-translate/core";
+import {By} from "@angular/platform-browser";
+
+describe('TitlePageComponent', () => {
+  let component: TitlePageComponent;
+  let fixture: ComponentFixture<TitlePageComponent>;
+  const title = 'testTitle';
+  const subTitle = 'testSubTitle';
+  const iconName = 'testIconName';
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [
+        TitlePageComponent,
+        TranslateModule.forRoot({})
+      ]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(TitlePageComponent);
+    component = fixture.componentInstance;
+    fixture.componentRef.setInput('title', title);
+    fixture.componentRef.setInput('subTitle', subTitle);
+  });
+
+  describe('title and sub title tests', () => {
+    beforeEach(async () => {
+      fixture.detectChanges();
+      await fixture.whenStable();
+    });
+
+    it('should create', async  () => {
+      expect(component).toBeTruthy();
+    });
+
+    it('should create a title', () => {
+      const element = fixture.debugElement.query(By.css('.title'));
+      expect(element).toBeTruthy();
+    });
+
+    it('should contain correct title', () => {
+      const titleText = fixture.debugElement.query(By.css('.title')).nativeElement.textContent;
+      expect(titleText).toBe(title);
+    });
+
+    it('should create a sub title', () => {
+      const element = fixture.debugElement.query(By.css('.subTitle'));
+      expect(element).toBeTruthy();
+    });
+
+    it('should contain correct sub title', () => {
+      const titleText = fixture.debugElement.query(By.css('.subTitle')).nativeElement.textContent;
+      expect(titleText).toBe(subTitle);
+    });
+  });
+
+  describe('icon tests', () => {
+    it('should create icon when icon is defined', async ()=> {
+      fixture.componentRef.setInput('iconName', iconName);
+      fixture.detectChanges();
+      await fixture.whenStable();
+
+      const element = fixture.debugElement.query(By.css('mat-icon'));
+      expect(element).toBeTruthy();
+    });
+
+    it('should NOT create icon when iconName is undefined', async() => {
+      fixture.componentRef.setInput('iconName', undefined);
+      fixture.detectChanges();
+      await fixture.whenStable();
+
+      const element = fixture.debugElement.query(By.css('mat-icon'));
+      expect(element).toBeFalsy();
+    });
+
+    it('should contain the correct iconName', async () => {
+      fixture.componentRef.setInput('iconName', iconName);
+      fixture.detectChanges();
+      await fixture.whenStable();
+
+      const iconText = fixture.debugElement.query(By.css('mat-icon')).nativeElement.textContent;
+      expect(iconText).toBe(iconName);
+    });
+  });
+});
