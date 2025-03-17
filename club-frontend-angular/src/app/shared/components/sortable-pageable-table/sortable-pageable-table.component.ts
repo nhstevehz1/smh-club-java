7921a31ac0d231ec1e3fcda990d6659c85b2fbd0
Steven Hernandez <smhinfo@comcast.net>
2025-03-16 23:38:36 -0400

    replaced @input with signals

--- club-frontend-angular/src/app/shared/components/sortable-pageable-table/sortable-pageable-table.component.ts
+++ club-frontend-angular/src/app/shared/components/sortable-pageable-table/sortable-pageable-table.component.ts
@@ -1,12 +1,10 @@
-import {AfterViewInit, Component, Input, ViewChild} from '@angular/core';
+import {AfterViewInit, Component, computed, input, ViewChild} from '@angular/core';
 import {ColumnDef} from "./models/column-def";
 import {MatTableDataSource, MatTableModule} from "@angular/material/table";
 import {MatSort, MatSortModule} from "@angular/material/sort";
 import {MatPaginator, MatPaginatorIntl, MatPaginatorModule} from "@angular/material/paginator";
-import {_, TranslatePipe, TranslateService} from "@ngx-translate/core";
-import {map} from "rxjs/operators";
-import {Observable, of} from "rxjs";
 import {CustomMatPaginatorIntlService} from "./services/custom-mat-paginator-intl.service";
+import {TranslatePipe} from "@ngx-translate/core";
 
 @Component({
   selector: 'app-sortable-pageable-table',
@@ -17,7 +15,6 @@ import {CustomMatPaginatorIntlService} from "./services/custom-mat-paginator-int
     TranslatePipe
   ],
   providers: [
-    TranslateService,
     {
       provide: MatPaginatorIntl,
       useClass: CustomMatPaginatorIntlService
@@ -27,11 +24,23 @@ import {CustomMatPaginatorIntlService} from "./services/custom-mat-paginator-int
   styleUrl: './sortable-pageable-table.component.scss'
 })
 export class SortablePageableTableComponent<T> implements AfterViewInit {
-  @Input({required: true}) columns!: Array<ColumnDef<T>>;
-  @Input({required: true}) dataSource!: MatTableDataSource<T>;
-  @Input() pageSizes: number[] = [5,10,25,100];
-  @Input() pageSize: number = 5;
-  @Input() resultsLength: number = 0;
+  columnsSignal
+      = input.required<Array<ColumnDef<T>>>({alias: 'columns'});
+
+  dataSourceSignal
+      = input.required<MatTableDataSource<T>>({alias: 'dataSource'});
+
+  pageSizesSignal
+      = input<Array<number>>([5,10,25,100], {alias: 'pageSizes'});
+
+  pageSizeSignal
+      = input<number>(5, {alias: 'pageSize'});
+
+  resultsLengthSignal
+      = input<number>(0, {alias: 'resultsLength'});
+
+  columnNamesSignal= computed<Array<string>>(() =>
+      this.columnsSignal().map(c => c.columnName));
 
   @ViewChild(MatSort, {static: true})
   sort!: MatSort;
@@ -39,41 +48,12 @@ export class SortablePageableTableComponent<T> implements AfterViewInit {
   @ViewChild(MatPaginator, {static: true})
   paginator!: MatPaginator;
 
-  get getColumnNames(): string[] {
-    return this.columns!.map(column => column.columnName);
-  }
-
-  constructor(private ts: TranslateService) {
-  }
   ngAfterViewInit() {
     // revert back to page 0 if it had been changed.
     this.sort!.sortChange.subscribe(() => this.paginator!.pageIndex = 0);
   }
 
-  translateDisplayName(column: ColumnDef<T>) : Observable<string> {
-    const translate = column.translateDisplayName || true;
-    if(translate) {
-      return this.ts.get(_(column.displayName)).pipe(
-          map((t) => t as string)
-      );
-    } else {
-      return of(column.displayName);
-    }
-  }
-
-  translateInstant(column: ColumnDef<T>): string {
-    const translate = column.translateDisplayName || true;
-    if(translate) {
-      const instant = this.ts.instant(_(column.displayName));
-      console.log('instant', instant);
-      return this.ts.instant(_(`'${column.displayName}'`));
-    } else {
-      return column.displayName;
-    }
-  }
-
   shouldTranslate(column: ColumnDef<T>): boolean {
     return column.translateDisplayName || true;
   }
-
 }
