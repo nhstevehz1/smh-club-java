085462127360b156fbfabf173d00adb38c608a58
Steven Hernandez <smhinfo@comcast.net>
2025-03-18 12:21:49 -0400

    fixed dto and service base bugs

--- club-api/src/main/java/com/smh/club/api/rest/dto/address/AddressFullNameDto.java
+++ club-api/src/main/java/com/smh/club/api/rest/dto/address/AddressFullNameDto.java
@@ -2,7 +2,6 @@ package com.smh.club.api.rest.dto.address;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.smh.club.api.rest.domain.annotations.SortAlias;
-import com.smh.club.api.rest.domain.annotations.SortExclude;
 import com.smh.club.api.rest.domain.annotations.SortTarget;
 import com.smh.club.api.rest.domain.entities.AddressEntity;
 import com.smh.club.api.rest.dto.FullNameDto;
@@ -24,10 +23,10 @@ import lombok.experimental.SuperBuilder;
 public class AddressFullNameDto extends AddressBaseDto {
 
   @JsonProperty("id")
-  @SortExclude
   private int id;
 
   @SortAlias("member.memberNumber")
+  @JsonProperty("member_number")
   private int memberNumber;
 
   @SortAlias("member.lastName")
