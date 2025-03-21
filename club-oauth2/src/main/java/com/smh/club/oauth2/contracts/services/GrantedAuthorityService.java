69be8c9c930b202e8e98e159da1fba62cff682d5
Steven Hernandez <smhinfo@comcast.net>
2025-03-21 09:14:01 -0400

    Removed projects (#51)
    Projects moved to other repos.

--- club-oauth2/src/main/java/com/smh/club/oauth2/contracts/services/GrantedAuthorityService.java
+++
@@ -1,9 +0,0 @@
-package com.smh.club.oauth2.contracts.services;
-
-import com.smh.club.oauth2.dto.RoleDto;
-
-public interface GrantedAuthorityService {
-
-  RoleDto addRole(long userId, RoleDto role);
-  void deleteRole(long userId, RoleDto role);
-}
