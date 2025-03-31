b9d04b10c06ce68ee802b0ab6473e7cc1896c94d
Steven Hernandez <smhinfo@comcast.net>
2025-03-31 13:27:26 -0400

    shorten dependency references paths

---
+++ client/src/app/shared/components/base-table-ex/testing/index.ts
@@ -0,0 +1,5 @@
+export * from './list-test/list-test.component';
+export * from './test-editor/test-editor.component';
+export * from './test-support';
+export * from './models';
+export * from './services';
---
+++ client/src/app/shared/components/base-table-ex/testing/services/index.ts
@@ -0,0 +1,3 @@
+export * from './test-edit-dialog.service';
+export * from './test.service';
+export * from './test-table.service';
