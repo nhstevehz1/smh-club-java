037aae24193bc041e0b46b506fc8aec6ab36519f
Steven Hernandez <smhinfo@comcast.net>
2025-03-31 11:41:12 -0400

    shorten dependency references paths

--- client/src/app/features/addresses/list-addresses/list-addresses.component.spec.ts
+++ client/src/app/features/addresses/list-addresses/list-addresses.component.spec.ts
@@ -1,22 +1,50 @@
-import {ListAddressesComponent} from './list-addresses.component';
-import {ComponentFixture, TestBed} from "@angular/core/testing";
-import {provideHttpClient} from "@angular/common/http";
-import {provideHttpClientTesting} from "@angular/common/http/testing";
-import {AddressService} from "../services/address.service";
-import {throwError} from "rxjs";
-import {generateAddressPagedData} from "../test/address-test";
-import {provideNoopAnimations} from "@angular/platform-browser/animations";
-import {PageRequest} from "../../../shared/models/page-request";
-import {asyncData} from "../../../shared/test-helpers/test-helpers";
-import {TranslateModule} from "@ngx-translate/core";
+import {ComponentFixture, fakeAsync, TestBed, tick} from '@angular/core/testing';
+import {provideHttpClient} from '@angular/common/http';
+import {provideHttpClientTesting} from '@angular/common/http/testing';
+import {provideNoopAnimations} from '@angular/platform-browser/animations';
+import {of, throwError} from 'rxjs';
+import {TranslateModule} from '@ngx-translate/core';
+
+import {AuthService} from '@app/core/auth';
+
+import {EditAction, EditDialogInput, EditDialogResult, EditEvent} from '@app/shared/components/edit-dialog';
+import {PageRequest} from '@app/shared/services/api-service';
+import {asyncData} from '@app/shared/testing';
+
+import {
+  ListAddressesComponent, AddressEditDialogService, AddressService, AddressTableService,
+  Address, AddressMember
+} from '@app/features/addresses';
+
+import {
+  generateAddress,
+  generateAddressColumnDefs,
+  generateAddressDialogInput,
+  generateAddressMember,
+  generateAddressPagedData
+} from '@app/features/addresses/testing';
 
 describe('ListAddressesComponent', () => {
   let fixture: ComponentFixture<ListAddressesComponent>;
   let component: ListAddressesComponent;
+
   let addressSvcMock: jasmine.SpyObj<AddressService>;
+  let authSvcMock: jasmine.SpyObj<AuthService>;
+  let dialogSvcMock: jasmine.SpyObj<AddressEditDialogService>;
+  let tableSvcMock: jasmine.SpyObj<AddressTableService>
+
+  const columnDefs = generateAddressColumnDefs();
 
   beforeEach(async () => {
-    addressSvcMock = jasmine.createSpyObj('AddressService', ['getAddresses']);
+    addressSvcMock = jasmine.createSpyObj('AddressService', [
+      'getPagedData', 'update', 'create', 'delete']);
+
+    dialogSvcMock = jasmine.createSpyObj('EditAddressDialogService',
+      ['openDialog', 'generateDialogInput']);
+
+    authSvcMock = jasmine.createSpyObj('AuthService', ['hasPermission']);
+    tableSvcMock = jasmine.createSpyObj('AddressTableService', ['getColumnDefs']);
+
     await TestBed.configureTestingModule({
       imports: [
           ListAddressesComponent,
@@ -24,67 +52,165 @@ describe('ListAddressesComponent', () => {
       ],
         providers: [
           {provide: AddressService, useValue: {}},
+          {provide: AuthService, useValue: {}},
+          {provide: AddressTableService, useValue: {}},
+          {provide: AddressEditDialogService, useValue: {}},
           provideHttpClient(),
           provideHttpClientTesting(),
           provideNoopAnimations()
       ],
-    }).overrideComponent(ListAddressesComponent,
-        { set: {providers: [{provide: AddressService, useValue: addressSvcMock}]}}
-    ).compileComponents();
-   fixture = TestBed.createComponent(ListAddressesComponent);
-   component = fixture.componentInstance;
+    }).overrideProvider(AddressService, {useValue: addressSvcMock})
+      .overrideProvider(AuthService, {useValue: authSvcMock})
+      .overrideProvider(AddressTableService, {useValue: tableSvcMock})
+      .overrideProvider(AddressEditDialogService, {useValue: dialogSvcMock})
+      .compileComponents();
+
+    fixture = TestBed.createComponent(ListAddressesComponent);
+    component = fixture.componentInstance;
   });
 
   describe('test component', () => {
     it('should create', () => {
         expect(component).toBeTruthy();
     });
-
-    it('should create column list', () => {
-        fixture.detectChanges();
-        expect(component.columns.length).toEqual(6);
-    });
   });
 
   describe('test service interactions on init', () => {
-    it('should call AddressService.getAddresses() on init', async () => {
-      const data = generateAddressPagedData(0, 5, 100);
-      addressSvcMock.getAddresses.and.returnValue(asyncData(data));
+    describe('AddressTableService', () => {
+      beforeEach(() => {
+        const data = generateAddressPagedData(0, 5, 1);
+        addressSvcMock.getPagedData.and.returnValue(asyncData(data));
+      });
 
-      fixture.detectChanges();
-      await fixture.whenStable();
-
-      const request = PageRequest.of(0, 5);
-      expect(addressSvcMock.getAddresses).toHaveBeenCalledWith(request);//With(request);
-    });
-
-    it('length should be set on init', async () => {
-      const data = generateAddressPagedData(0, 5, 100);
-      addressSvcMock.getAddresses.and.returnValue(asyncData(data));
-
-      fixture.detectChanges();
-      await fixture.whenStable();
-
-      expect(component.resultsLength).toEqual(data.page.totalElements);
-    });
-
-    it('datasource.data should be set on init', async () => {
-        const data = generateAddressPagedData(0, 5, 2);
-        addressSvcMock.getAddresses.and.returnValue(asyncData(data));
+      it('should call AddressTableService.getColumnDefs', async () => {
+        const spy = tableSvcMock.getColumnDefs.and.returnValue(columnDefs);
 
         fixture.detectChanges();
         await fixture.whenStable();
 
-        expect(component.datasource().data).toBe(data._content);
+        expect(spy).toHaveBeenCalled();
+      });
+
+      it('should create correct column list', async () => {
+        tableSvcMock.getColumnDefs.and.returnValue(columnDefs);
+
+        fixture.detectChanges();
+        await fixture.whenStable();
+
+        expect(component.columns()).toEqual(columnDefs);
+      });
     });
 
-    it('datasource.data should be empty when an error occurs while calling getAddresses', async () => {
-        addressSvcMock.getAddresses.and.returnValue(throwError(() => 'error'));
+    describe('test AddressService.getPagedData interactions', () => {
+      beforeEach(() => {
+        tableSvcMock.getColumnDefs.and.returnValue(columnDefs);
+      });
+
+      it('should call AddressService.getPagedData', async () => {
+        const data = generateAddressPagedData(0, 5, 100);
+        addressSvcMock.getPagedData.and.returnValue(asyncData(data));
+
+        fixture.detectChanges();
+        await fixture.whenStable();
+
+        const request = PageRequest.of(0, 5);
+        expect(addressSvcMock.getPagedData).toHaveBeenCalledWith(request);//With(request);
+      });
+
+      it('should set the correct data length', async () => {
+        const data = generateAddressPagedData(0, 5, 100);
+        addressSvcMock.getPagedData.and.returnValue(asyncData(data));
+
+        fixture.detectChanges();
+        await fixture.whenStable();
+
+        expect(component.resultsLength()).toEqual(data.page.totalElements);
+      });
+
+      it('should set correct datasource.data', async () => {
+        const data = generateAddressPagedData(0, 5, 2);
+        addressSvcMock.getPagedData.and.returnValue(asyncData(data));
+
+        fixture.detectChanges();
+        await fixture.whenStable();
+
+        expect(component.datasource().data).toEqual(data._content);
+      });
+
+      it('datasource.data should be empty when an error occurs while calling getPagedData', async () => {
+        addressSvcMock.getPagedData.and.returnValue(throwError(() => 'error'));
 
         fixture.detectChanges();
         await fixture.whenStable();
 
         expect(component.datasource().data).toEqual([]);
+      });
+    });
+  });
+
+  describe('test dialog interactions', () => {
+    let editEvent: EditEvent<AddressMember>;
+    let dialogInput: EditDialogInput<Address>;
+    let dialogResult: EditDialogResult<Address>;
+
+    beforeEach(() => {
+      const data = generateAddressPagedData(0, 5, 1);
+      addressSvcMock.getPagedData.and.returnValue(asyncData(data));
+      tableSvcMock.getColumnDefs.and.returnValue(columnDefs);
+
+      editEvent = {
+        idx: 0,
+        data: generateAddressMember(0)
+      }
+      const address = generateAddress();
+      dialogInput = generateAddressDialogInput(address, EditAction.Edit);
+      dialogResult = {context: address, action: EditAction.Cancel};
+    });
+
+    describe('onDelete', () => {
+      it('onDeleteClick should call AddressDialogService.generateDialogInput', fakeAsync(() => {
+        dialogSvcMock.openDialog.and.returnValue(of(dialogResult));
+        const spy =
+          dialogSvcMock.generateDialogInput.and.returnValue(dialogInput);
+
+        component.onDeleteClick(editEvent);
+        tick();
+
+        expect(spy).toHaveBeenCalled();
+      }));
+
+      it('onDeleteClick should call AddressEditDialogService.openDialog', fakeAsync(() => {
+        dialogResult.action = EditAction.Cancel;
+        const spy =
+          dialogSvcMock.openDialog.and.returnValue(of(dialogResult));
+
+        component.onDeleteClick(editEvent);
+        tick();
+
+        expect(spy).toHaveBeenCalled();
+      }));
+
+      it('onDeleteClick should NOT call AddressService.update when EditAddressDialogService.open return cancel', fakeAsync(() => {
+        dialogResult.action = EditAction.Cancel;
+        dialogSvcMock.openDialog.and.returnValue(of(dialogResult));
+        const spy = addressSvcMock.update.and.stub();
+
+        component.onDeleteClick(editEvent);
+        tick();
+
+        expect(spy).toHaveBeenCalledTimes(0);
+      }));
+
+      it('onDeleteClick should NOT call AddressService.deleteAddress when EditAddressDialogService.open return cancel', fakeAsync(() => {
+        dialogResult.action = EditAction.Cancel;
+        dialogSvcMock.openDialog.and.returnValue(of(dialogResult));
+        const spy = addressSvcMock.delete.and.stub();
+
+        component.onDeleteClick(editEvent);
+        tick();
+
+        expect(spy).toHaveBeenCalledTimes(0);
+      }));
     });
   });
 });
--- client/src/app/features/addresses/list-addresses/list-addresses.component.ts
+++ client/src/app/features/addresses/list-addresses/list-addresses.component.ts
@@ -1,38 +1,45 @@
-import {AfterViewInit, Component, ViewChild, ViewEncapsulation} from '@angular/core';
-import {
-  SortablePageableTableComponent
-} from "../../../shared/components/sortable-pageable-table/sortable-pageable-table.component";
-import {AddressService} from "../services/address.service";
-import {ColumnDef} from "../../../shared/components/sortable-pageable-table/models/column-def";
-import {Address, AddressMember} from "../models/address";
-import {BaseTableComponent} from "../../../shared/components/base-table-component/base-table-component";
-import {first, merge, Observable} from "rxjs";
-import {startWith, switchMap} from "rxjs/operators";
-import {AuthService} from '../../../core/auth/services/auth.service';
-import {MatDialog} from '@angular/material/dialog';
-import {EditAction, EditDialogData, EditEvent} from '../../../shared/components/edit-dialog/models/edit-event';
-import {AddressEditorComponent} from '../address-editor/address-editor.component';
+import {AfterViewInit, Component, OnInit, ViewChild, ViewEncapsulation} from '@angular/core';
 import {HttpErrorResponse} from '@angular/common/http';
-import {EditDialogComponent} from '../../../shared/components/edit-dialog/edit-dialog.component';
-import {PagedData} from '../../../shared/models/paged-data';
+import {first, merge, mergeMap, Observable} from 'rxjs';
+import {startWith, switchMap} from 'rxjs/operators';
+
+import {AuthService} from '@app/core/auth';
+
+import {SortablePageableTableComponent} from '@app/shared/components/sortable-pageable-table';
+import {ColumnDef} from '@app/shared/components/sortable-pageable-table';
+import {BaseTableComponent} from '@app/shared/components/base-table-component/base-table-component';
+import {PagedData} from '@app/shared/services/api-service';
+import {EditAction, EditDialogResult, EditEvent} from '@app/shared/components/edit-dialog';
+
+import {
+  AddressEditDialogService, AddressService, AddressTableService,
+  Address, AddressMember
+} from '@app/features/addresses';
+
+
 
 @Component({
   selector: 'app-list-addresses',
   imports: [SortablePageableTableComponent],
-  providers: [AddressService, AddressService],
+  providers: [AddressService, AddressTableService, AddressEditDialogService],
   templateUrl: './list-addresses.component.html',
   styleUrl: './list-addresses.component.scss',
   encapsulation: ViewEncapsulation.ShadowDom
 })
-export class ListAddressesComponent extends BaseTableComponent<AddressMember> implements AfterViewInit {
+export class ListAddressesComponent extends BaseTableComponent<AddressMember> implements OnInit, AfterViewInit {
+
   @ViewChild(SortablePageableTableComponent, {static: true})
   private _table!: SortablePageableTableComponent<ColumnDef<AddressMember>>;
 
-  constructor(private svc: AddressService,
-              auth: AuthService,
-              private dialog: MatDialog) {
+  constructor(auth: AuthService,
+              private svc: AddressService,
+              private dialogSvc: AddressEditDialogService,
+              private tableSvc: AddressTableService) {
     super(auth);
-    this.columns.set(this.svc.getColumnDefs());
+  }
+
+  ngOnInit() {
+    this.columns.set(this.tableSvc.getColumnDefs());
   }
 
   ngAfterViewInit(): void {
@@ -47,11 +54,11 @@ export class ListAddressesComponent extends BaseTableComponent<AddressMember> im
   }
 
   onEditClick(event: EditEvent<AddressMember>): void {
-    this.openDialog(event, EditAction.Edit);
+    this.openDialog('addresses.list.dialog.update', event, EditAction.Edit);
   }
 
   onDeleteClick(event: EditEvent<AddressMember>): void {
-    this.openDialog(event, EditAction.Delete);
+    this.openDialog('addresses.list.dialog.update', event, EditAction.Delete);
   }
 
   private getCurrentPage(): Observable<PagedData<AddressMember>> {
@@ -59,49 +66,37 @@ export class ListAddressesComponent extends BaseTableComponent<AddressMember> im
     const pr = this.getPageRequest(
       this._table.paginator.pageIndex, this._table.paginator.pageSize,
       this._table.sort.active, this._table.sort.direction);
-    return this.svc.getAddresses(pr).pipe(first());
+    return this.svc.getPagedData(pr).pipe(first());
   }
 
-  private openDialog(event: EditEvent<AddressMember>, action: EditAction): void {
-   const dialogData: EditDialogData<Address> = {
-     title: 'Address Title',
-     component: AddressEditorComponent,
-     form: this.svc.generateAddressForm(),
-     context: event.data as Address,
-     action: action
-    }
+  private openDialog(title: string, event: EditEvent<AddressMember>, action: EditAction): void {
+   const dialogInput = this.dialogSvc.generateDialogInput(title, event.data as Address, action);
 
-    const dialogRef =
-      this.dialog.open<EditDialogComponent<Address>, EditDialogData<Address>>(
-        EditDialogComponent<Address>, {data: dialogData});
-
-    dialogRef.afterClosed().subscribe({
-      next: (result: EditDialogData<Address>) => {
+    this.dialogSvc.openDialog(dialogInput).subscribe({
+      next: (result: EditDialogResult<Address>) =>  {
         if(result.action == EditAction.Edit) {
           this.updateAddress(result.context);
-        } else if (result.action == EditAction.Delete) {
-          this.deleteEmail(event.data.id);
+        } else if(result.action == EditAction.Delete) {
+          this.deleteAddress(event.data.id);
         }
       }
     });
   }
 
   private updateAddress(address: Address): void {
-    this.svc.updateAddress(address).subscribe({
-      next: () => this.getCurrentPage().subscribe({
-        next: data => this.processPageData(data),
-        error: (err: HttpErrorResponse) => this.processRequestError(err)
-      }),
-      error: (err: HttpErrorResponse)=> this.processRequestError(err)
-    });
+    this.svc.update(address.id, address).pipe(
+      mergeMap(() => this.getCurrentPage())
+    ).subscribe({
+      next: data => this.processPageData(data),
+      error: (err: HttpErrorResponse) => this.processRequestError(err)
+    })
   }
 
-  private deleteEmail(id: number) {
-    this.svc.deleteAddress(id).subscribe({
-      next: () => this.getCurrentPage().subscribe({
-        next: data => this.processPageData(data),
-        error: (err: HttpErrorResponse)=> this.processRequestError(err)
-      }),
+  private deleteAddress(id: number) {
+    this.svc.delete(id).pipe(
+      mergeMap(() => this.getCurrentPage())
+    ).subscribe({
+      next: data => this.processPageData(data),
       error: (err: HttpErrorResponse) => this.processRequestError(err)
     });
   }
