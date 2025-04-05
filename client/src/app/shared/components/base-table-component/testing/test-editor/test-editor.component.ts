f08d8b882d296f8dd3fe78b435835cef703a5b64
Steven Hernandez <smhinfo@comcast.net>
2025-04-05 12:52:45 -0400

    removed unused test components

--- client/src/app/shared/components/base-table-component/testing/test-editor/test-editor.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-test-editor',
-  imports: [],
-  templateUrl: './test-editor.component.html',
-  styleUrl: './test-editor.component.scss'
-})
-export class TestEditorComponent {}
