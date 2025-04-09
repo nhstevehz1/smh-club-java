b0018952d5d9fb64f4955be5fad6757fb0c84b6f
Steven Hernandez <smhinfo@comcast.net>
2025-04-09 10:49:22 -0400

    updated app.config

--- client/src/app/app.config.ts
+++ client/src/app/app.config.ts
@@ -1,5 +1,5 @@
 import {ApplicationConfig, inject, provideAppInitializer, provideZoneChangeDetection} from '@angular/core';
-import {provideRouter} from '@angular/router';
+import {provideRouter, withComponentInputBinding} from '@angular/router';
 import {provideHttpClient, withInterceptors} from '@angular/common/http';
 import {DOCUMENT} from '@angular/common';
 import {provideAnimations} from '@angular/platform-browser/animations';
@@ -16,7 +16,7 @@ export const appConfig: ApplicationConfig = {
   providers: [
     provideAnimations(),
     provideZoneChangeDetection({eventCoalescing: true}),
-    provideRouter(routes),
+    provideRouter(routes, withComponentInputBinding()),
     provideHttpClient(
         withInterceptors([
           loadingSpinnerInterceptor,
