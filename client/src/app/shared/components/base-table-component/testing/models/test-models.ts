b6d21049451c579c333ff62aada43d1ce1a2eff4
Steven Hernandez <smhinfo@comcast.net>
2025-04-16 15:01:12 -0400

    removed 'create' models

--- client/src/app/shared/components/base-table-component/testing/models/test-models.ts
+++ client/src/app/shared/components/base-table-component/testing/models/test-models.ts
@@ -1,7 +1,5 @@
 import {Updatable} from '@app/shared/models/updatable';
 
-export interface TableModelCreate {
+export interface TableModel extends Updatable {
   tableField: string,
 }
-
-export interface TableModel extends TableModelCreate, Updatable {}
--- client/src/app/shared/components/base-table-component/testing/services/mock-table-api.service.ts
+++ client/src/app/shared/components/base-table-component/testing/services/mock-table-api.service.ts
@@ -1,9 +1,9 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
 import {HttpClient} from '@angular/common/http';
 
 import {BaseApiService} from '@app/shared/services/api-service/base-api.service';
 
-import {TableModelCreate, TableModel} from '@app/shared/components/base-table-component/testing/models/test-models';
+import {TableModel} from '@app/shared/components/base-table-component/testing/models/test-models';
 
 @Injectable()
 export class MockTableApiService extends BaseApiService<TableModel, TableModel> {
