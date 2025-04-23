6210e339ced31b98874b61b63f68a2208e3d6328
Steven Hernandez <smhinfo@comcast.net>
2025-04-23 07:39:37 -0400

    added base viewlist

---
+++ client/src/app/shared/components/base-view-list/base-view-list.ts
@@ -0,0 +1,108 @@
+import {Updatable} from '@app/shared/models/updatable';
+import {CrudService} from '@app/shared/services/api-service/crud-service';
+import {Editor} from '@app/shared/components/base-editor/editor';
+import {EditDialogService} from '@app/shared/services/dialog-edit-service/edit-dialog-service';
+import {EditAction} from '@app/shared/components/base-edit-dialog/models';
+import {model} from '@angular/core';
+import {mergeMap, map} from 'rxjs/operators';
+import {of} from 'rxjs';
+
+export abstract class BaseViewList<T extends Updatable, C extends Editor<T>> {
+
+  items = model<T[]>([]);
+  error = model<string>();
+
+  constructor(private apiSvc: CrudService<T>,
+              private dialogSvc: EditDialogService<T, C>) {}
+
+
+  protected processAction(title: string, context: T, action: EditAction): void {
+    if(action == EditAction.Create) {
+      this.processCreate(title, context);
+    } else if( action == EditAction.Edit) {
+      this.processEdit(title, context);
+    } else if (action == EditAction.Delete) {
+      this.processDelete(title, context);
+    }
+  }
+
+  private processCreate(title: string, context: T): void {
+    const input = this.dialogSvc.generateDialogInput(title, context, EditAction.Create);
+
+    this.dialogSvc.openDialog(input).pipe(
+      mergeMap(result => {
+        if(result.action == EditAction.Create) {
+          return this.apiSvc.create(result.context);
+        } else return of(null);
+      })
+    ).subscribe({
+      next: data => {
+        if(data) {
+          this.items.update((list) => [data, ...list]);
+        }
+      },
+      error: err => console.debug(err) // TODO: error handling
+    })
+  }
+
+  private processEdit(title: string, context: T): void {
+    const input = this.dialogSvc.generateDialogInput(title, context, EditAction.Edit);
+
+    this.dialogSvc.openDialog(input).pipe(
+      mergeMap(result => {
+        if(result.action == EditAction.Edit) {
+          return this.apiSvc.update(result.context);
+        }  else {
+          return of(null);
+        }
+      })
+    ).subscribe({
+      next: result => {
+        if (result) {
+          this.updateList(result);
+        }
+      }, error: err => console.debug(err) // todo error handling
+    });
+  }
+
+  private processDelete(title: string, context: T): void {
+    const input = this.dialogSvc.generateDialogInput(title, context, EditAction.Delete);
+
+    this.dialogSvc.openDialog(input).pipe(
+      mergeMap(result => {
+        if(result.action == EditAction.Delete) {
+          return this.apiSvc.delete(result.context.id).pipe(map(() => result));
+        } else {
+          return of(result);
+        }
+      })
+    ).subscribe({
+      next: result => {
+        if(result.action == EditAction.Delete) {
+          this.deleteFromList(result.context);
+        }
+      }, error: err => console.debug(err) // TODO: error handling
+    });
+
+  }
+
+  private getItemIndex(id: number): number {
+    return this.items().findIndex(item => item.id == id);
+  }
+
+  private updateList(item: T): void {
+    const index = this.getItemIndex(item.id);
+    this.items.update(list => {
+      list[index] = item;
+      return list;
+    });
+  }
+
+  private deleteFromList(item: T): void {
+    const index = this.getItemIndex(item.id);
+    this.items.update(list => {
+      list.splice(index, 1);
+      return list;
+    })
+  }
+}
--- client/src/app/shared/services/api-service/base-api.service.ts
+++ client/src/app/shared/services/api-service/base-api.service.ts
@@ -5,14 +5,13 @@ import {CrudService} from '@app/shared/services/api-service/crud-service';
 import {PageRequest, PagedData} from '@app/shared/services/api-service/models';
 import {Updatable} from '@app/shared/models/updatable';
 import {PagedDataService} from '@app/shared/services/api-service/paged-data-service';
+import {BaseCrudService} from '@app/shared/services/api-service/base-crud.service';
 
-export abstract class BaseApiService<T extends Updatable, P extends T> implements CrudService<T>, PagedDataService<P> {
-
-  readonly baseUri: string;
+export abstract class BaseApiService<T extends Updatable, P extends T> extends BaseCrudService<T> implements PagedDataService<P> {
 
   protected constructor(baseUri: string,
-                        protected http: HttpClient) {
-    this.baseUri = baseUri;
+                        http: HttpClient) {
+    super(baseUri, http);
   }
 
   getPagedData(pageRequest: PageRequest): Observable<PagedData<P>> {
@@ -20,25 +19,5 @@ export abstract class BaseApiService<T extends Updatable, P extends T> implement
     const uri = query == null ? `${this.baseUri}/page`: `${this.baseUri}/page${query}`;
 
     return this.http.get<PagedData<P>>(uri);
-  }
-
-  get(id: number) : Observable<T> {
-    return this.http.get<T>(`${this.baseUri}/${id}`);
-  }
-
-  getAll(): Observable<T[]> {
-    return this.http.get<T[]>(`${this.baseUri}/all`)
-  }
-
-  create(create: T): Observable<T> {
-    return this.http.post<T>(this.baseUri, create);
-  }
-
-  update(update: T): Observable<T> {
-    return this.http.put<T>(`${this.baseUri}/${update.id}`, update );
-  }
-
-  delete(id: number): Observable<void> {
-    return this.http.delete<void>(`${this.baseUri}/${id}`);
   }
 }
